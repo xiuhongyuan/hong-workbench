a4db1ffd58d7591254de8762aa1d68315d78f4a8
xiuhongyuan <yuanxh1996@163.com>
2026-07-28 00:02:23 +0800

    deploy sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+// 离线缓存 Service Worker
+const CACHE = 'workbench-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.webmanifest',
+  'icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  // 仅缓存同源静态资源；云端同步请求（jsonbin）直接走网络
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return resp;
+    }).catch(() => hit))
+  );
+});
